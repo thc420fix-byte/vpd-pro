ed94b2ca6dbbac593e4032b4168eceee7567cb27
VPD PRO <vpd@pro.app>
2026-04-08 23:59:08 +0900

    Initial commit: VPD PRO PWA

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_VERSION = 'vpd-pro-v3';
+const ASSETS = [
+  './index.html',
+  './icon-192.png',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(c => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(response => {
+        if (!response || response.status !== 200 || response.type === 'opaque') {
+          return response;
+        }
+        const clone = response.clone();
+        caches.open(CACHE_VERSION).then(c => c.put(e.request, clone));
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
